629ef13061169ebc251d54c6ee0b6022ae149d62
Apurba Kumar Pramanik <59066595+uakp98@users.noreply.github.com>
2020-06-22 13:01:44 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -125,7 +125,7 @@ var penelope = function() {
 	
 	/*Typed js start*/
 	$("#typed").typed({
-		strings: ["^2100&nbsp;I Am Apurba,<br/>B.Tech in Computer Science &#38; Engineering."],
+		strings: ["^2100&nbsp;I Am Apurba,<br/>An Web Developer,<br/>B.Tech in Computer Sci. &#38; Engg."],
 		typeSpeed: 25,
 		backDelay: 750,
 		loop: false,
